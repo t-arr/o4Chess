44ef3058cd403f5800b7f48faaf804a5eff3b16c
tuure <tuure.ylitolonen@gmail.com>
2023-03-26 13:42:41 +0300

    added quit popup

--- src/main/java/com/example/chess/MainMenuController.java
+++ src/main/java/com/example/chess/MainMenuController.java
@@ -10,19 +10,28 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class MainMenuController {
     private HostServices hostServices;
-
-    //Teesti123
 
     @FXML
     public void switchScreen(ActionEvent event) throws IOException {
         String btnId = ((Button) event.getSource()).getId();
         if(btnId.equalsIgnoreCase("quit")){
-            Platform.exit();
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Confirm Exit");
+            alert.setHeaderText("Are you sure you want to quit the game?");
+            alert.setContentText("Press OK to exit, or Cancel to go back");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK){
+                Platform.exit();
+            }
         } else if (btnId.equalsIgnoreCase("rules")) {
             handleRulesButton();
         }else{
