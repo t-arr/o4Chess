2f685d6edfab938d841f7047a652137302249f9f
tuure <tuure.ylitolonen@gmail.com>
2023-04-05 22:20:00 +0300

    implemented base for movement

--- src/main/java/com/example/chess/Board.java
+++ src/main/java/com/example/chess/Board.java
@@ -14,8 +14,17 @@ public class Board {
                 {"wrook", "wknight", "wbishop", "wqueen", "wking", "wbishop", "wknight", "wrook"}};
     }
 
+    public void swap(int[] from, int[] to){
+        String temp = board[from[0]][from[1]];
+        board[from[0]][from[1]] = "-";
+        board[to[0]][to[1]] = temp;
+    }
     public String[][] getBoard(){
         return board;
+    }
+
+    public String getPiece(int[] coords){
+        return board[coords[0]][coords[1]];
     }
 
 }
--- src/main/java/com/example/chess/chessController.java
+++ src/main/java/com/example/chess/chessController.java
@@ -1,19 +1,20 @@
 package com.example.chess;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 public class chessController {
+    private final Board BOARD = new Board();
     @FXML
     private GridPane boardGrid;
-    private final Board BOARD = new Board();
-
+    private String piece = null;
+    private Button prevButton = null;
     @FXML
     public void initialize() {
-        String[][] state = BOARD.getBoard();
         boolean isWhite = true;
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
@@ -24,14 +25,7 @@ public class chessController {
                 } else {
                     button.getStyleClass().add("black-button");
                 }
-                if(!state[row][col].equals("-")){
-                    String imageUrl = System.getProperty("user.dir") + "/src/main/java/com/example/chess/pawns/" + state[row][col] + ".png";
-                    Image image = new Image(imageUrl);
-                    ImageView imageView = new ImageView(image);
-                    imageView.setFitWidth(50);
-                    imageView.setFitHeight(50);
-                    button.setGraphic(imageView);
-                }
+                drawImages(button, row, col);
                 boardGrid.add(button, col, row);
                 isWhite = !isWhite;
                 button.setOnAction(e -> printCoordinates(button));
@@ -40,9 +34,39 @@ public class chessController {
         }
         boardGrid.setGridLinesVisible(true);
     }
+
+    private void drawImages(Button button, int row, int col) {
+        String[][] state = BOARD.getBoard();
+
+        if (!state[row][col].equals("-")) {
+            String imageUrl = System.getProperty("user.dir") + "/src/main/java/com/example/chess/pawns/" + state[row][col] + ".png";
+            Image image = new Image(imageUrl);
+            ImageView imageView = new ImageView(image);
+            imageView.setFitWidth(50);
+            imageView.setFitHeight(50);
+            button.setGraphic(imageView);
+        }else {
+            button.setGraphic(null);
+        }
+    }
+
     private void printCoordinates(Button button) {
-        int row = GridPane.getRowIndex(button);
-        int col = GridPane.getColumnIndex(button);
-        System.out.println("Button pressed at row " + row + ", column " + col);
+        int[] to = new int[]{GridPane.getRowIndex(button), GridPane.getColumnIndex(button)};
+        if(prevButton == null){
+            prevButton = button;
+        }else{
+            BOARD.swap(new int[]{GridPane.getRowIndex(prevButton), GridPane.getColumnIndex(prevButton)}, to);
+            updateBoardGUI();
+            prevButton = null;
+        }
+    }
+    private void updateBoardGUI() {
+        for (Node node : boardGrid.getChildren()) {
+            if (node instanceof Button button) {
+                int row = GridPane.getRowIndex(button);
+                int col = GridPane.getColumnIndex(button);
+                drawImages(button, row, col);
+            }
+        }
     }
 }
